3fc60670eefffbc892d42f9ac95df59924683e53
Andrew <expandrew@gmail.com>
2017-03-16 20:59:30 -0700

    implement if/else statement to account for user inputs of 3, 5, and 15 resulting in outputs of Ping, Pong, and PingPong as well as change message indicating input was not a number from a div in index.html to an alert in scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
 //business logic
 var result = [];
 
+function resetFields()  {
+  $("input#userInput").val();
+};
+
 //user interface logic
 $(document).ready(function(event){
   $("form#userInput").submit(function(event){
     event.preventDefault();
     var userInput = parseInt($("input#userInput").val());
-    console.log(userInput);
     if (Number.isInteger(userInput) === false ) {
-      $("#NaN").show();
+      $(alert("Please enter a number!"));
+    } else if ((userInput) % 15 === 0) {
+        $("ul#pingpong").append("<li>" + "Ping-Pong!" + "</li>");
+    } else if ((userInput) % 5 === 0) {
+        $("ul#pingpong").append("<li>" + "Pong!" + "</li>");
+    }  else if ((userInput) % 3 === 0)  {
+        $("ul#pingpong").append("<li>" + "Ping!" + "</li>");
     };
   });
 });
